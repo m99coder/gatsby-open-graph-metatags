323de784f33c50686900114cfd8395d90fa442cb
Marco Lehmann <m99@posteo.de>
2022-01-22 18:29:29 +0100

    add sentry

--- src/utils/wrap-page-element.js
+++ src/utils/wrap-page-element.js
@@ -1,15 +1,18 @@
 const React = require("react")
 const { IntlProvider } = require("react-intl")
+const Sentry = require("@sentry/gatsby")
 
 const { default: Layout } = require("../components/layout")
 
 // Wraps every page in a component
 exports.wrapPageElement = ({ element, props }) => {
   return (
-    <IntlProvider
-        locale={"de"}
-        messages={{ "message.key": "Message Value" }}>
-      <Layout {...props}>{element}</Layout>
-    </IntlProvider>
+    <Sentry.ErrorBoundary fallback={"An error has occurred"}>
+      <IntlProvider
+          locale={"de"}
+          messages={{ "message.key": "Message Value" }}>
+        <Layout {...props}>{element}</Layout>
+      </IntlProvider>
+    </Sentry.ErrorBoundary>
   )
 }
